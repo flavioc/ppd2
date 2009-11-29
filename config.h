aee7a9906b02b3028c2d6b2415cee7cd3dbe57bc
Flávio Cruz <flaviocruz@gmail.com>
2009-11-29 03:29:11 +0000

    esqueleto de threads.

--- config.h
+++ config.h
@@ -5,6 +5,8 @@
 
 #ifdef MULTITHREAD
 #include "pthread.h"
+
+#define LINE_PER_THREAD 2
 #endif
 
 #endif
--- simulator.c
+++ simulator.c
@@ -2,8 +2,50 @@
 #include <stdlib.h>
 
 #include "map.h"
+#include "thread.h"
+
+#define MASTER_THREAD 0
+#define THREAD_PARAM void*
 
 static int nthreads = 4;
+static ThreadData* thread_data;
+
+static THREAD_PARAM
+run_thread(THREAD_PARAM data)
+{
+  int thread_num = (int)data;
+  ThreadData *mydata = thread_data + thread_num;
+  
+  printf("Sou a thread %d\n", thread_num);
+  
+  if(thread_num != MASTER_THREAD)
+    pthread_exit(NULL);
+    
+  return NULL;
+}
+
+static void
+simulate(void)
+{
+  // create thread data
+  thread_data = (ThreadData*)malloc(sizeof(ThreadData) * nthreads);
+  
+  // create worker threads
+  int i;
+  pthread_t tids[nthreads];
+  
+  for(i = 1; i < nthreads; ++i) {
+    pthread_create(&tids[i], NULL, run_thread, (void*)i);  
+  }
+  
+  run_thread((THREAD_PARAM)MASTER_THREAD);
+  
+  for(i = 1; i < nthreads; ++i) {
+    pthread_join(tids[i], NULL);
+  }
+  
+  free(thread_data);
+}
 
 int
 main()
@@ -11,6 +53,8 @@ main()
   map = map_read(stdin);
   
   map_print(map);
+  
+  simulate();
   
   map_free(map);
   
