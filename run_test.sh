af9bc0c2434a4c6125246bc31ae7be26958d86ef
Flávio Cruz <flaviocruz@gmail.com>
2009-12-13 18:49:36 +0000

    test script

---
+++ run_test.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+DIR=results
+
+run_prog()
+{
+  PROGRAM="$1"
+  FILE="$2"
+  RESULT="$3"
+
+  echo "$PROGRAM < $FILE > $DIR/$RESULT"
+
+  { time $PROGRAM < $FILE; } &> $DIR/$RESULT
+}
+
+run_test()
+{
+  FILE=$1
+  PATH="files/$FILE"
+
+  run_prog "./seq" "$PATH" "sequential" || exit 1
+  run_prog "./simulator 1" "$PATH" "1thread" || exit 1
+  run_prog "./simulator 2" "$PATH" "2thread" || exit 1
+  run_prog "./simulator 4" "$PATH" "4thread" || exit 1
+}
+
+mkdir -p $DIR
+run_test teste1 || exit 1
+run_test teste2 || exit 1
+run_test teste3 || exit 1
+
